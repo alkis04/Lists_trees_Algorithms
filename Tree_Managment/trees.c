b0a12aa11aa2f4e0cfdd08f2f59e5268f91ab0c4
alkis04 <alkishon@gmail.com>
2023-02-09 00:13:47 +0200

    addtree

--- Tree_Managment/trees.c
+++ Tree_Managment/trees.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct treenode
+{
+    int data;
+    struct treenode *left;
+    struct treenode *right;
+};
+
+typedef struct treenode *Treeptr;
+
+Treeptr addtree(Treeptr tree, int d)
+{
+    if(tree == NULL)
+    {
+        Treeptr new_node = malloc(sizeof(struct treenode));
+        new_node->data = d;
+        new_node->left = NULL;
+        new_node->right = NULL;
+    }
+    else if(d > tree->data)
+    {
+
+    }
+}
+
 int main()
 {
 
